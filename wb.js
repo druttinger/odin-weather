6a0dde32bed967d970266f735ee00bdcc54cdcd7
David Ruttinger <192172161+druttinger@users.noreply.github.com>
2025-02-17 15:17:48 -0700

    Create full functionality

--- wb.js
+++ wb.js
@@ -2,41 +2,75 @@ import { WeatherBox } from "./weather";
 
 let wb = new WeatherBox();
 
-const currentButton = document.getElementById('current-location');
-currentButton.addEventListener('click', wb.setCurrentLocationHere, wb);
+const cityField = document.getElementById('city');
 
 const setLocationButton = document.getElementById('set-location');
-setLocationButton.addEventListener('click', () => {
-    wb.setLocationByName(prompt('Name a location'));
+setLocationButton.addEventListener('click', async () => {
+    locationViewer.innerText = 'Loading...';
+    weatherViewer.innerText = '';
+    await wb.setLocationByName(cityField.value);
+    refresh();
 });
 
+document.addEventListener('keypress', async (event) => {
+    if (event.key === 'Enter'){
+        locationViewer.innerText = 'Loading...';
+        weatherViewer.innerText = '';
+        await wb.setLocationByName(cityField.value);
+        refresh();
+    }
+});
 
+const currentButton = document.getElementById('current-location');
+currentButton.addEventListener('click', async () => {
+    locationViewer.innerText = 'Loading...';
+    weatherViewer.innerText = '';
+    await wb.setCurrentLocationHere();
+    refresh();
+});
 
+const info = document.getElementById('info');
 const locationViewer = document.getElementById('locationView');
 const weatherViewer = document.getElementById('weatherView');
 
-wb.resetWeather();
+const unitSelector = document.getElementById('unit-selector');
+unitSelector.addEventListener('click', (event)=> {
+    let elem = event.target;
+    if (elem.getAttribute('name') === 'temp-unit'){
+        wb.setTempUnits(elem.getAttribute('value'))
+        refresh();
+    } else if (elem.getAttribute('name') === 'wind-unit'){
+        wb.setWindUnits(elem.getAttribute('value'))
+        refresh();
+    }
+})
 
-function refresh(){
-    // console.log("calling refresh()")
-    // console.log(wb.locationName);
+
+async function refresh(){
+    await wb.loadLatestWeather();
+    cityField.value = wb.getCityName();
     locationViewer.innerText = ('Weather for ' + wb.getLocation() + 
         ' at latitude ' + wb.latitude + ', longitude ' + wb.longitude);
-    
+    let temp = wb.weatherData.current.temperature_2m;
+    let precipitation = wb.weatherData.current.precipitation;
+    let windSpeed = wb.weatherData.current.wind_speed_10m;
+
     weatherViewer.innerText = 'It is currently ' + 
-                                wb.weatherData.current.temperature_2m +
-                                'degrees farenheit with ' +
-                                wb.weatherData.current.precipitation_sum +
-                                'mm of precipitation.';
+                                temp +
+                                ' degrees ' + wb.tempUnits + ' with ' +
+                                (precipitation ? 
+                                 precipitation + 'mm of':
+                                 'no') +
+                                ' precipitation. The wind speed is ' +
+                                windSpeed + wb.windUnits +'.';
+
+    if (temp < 37){
+        info.className = 'snowy'
+    } else if (precipitation > 0){
+        info.className = 'rainy'
+    } else {
+        info.className = 'sunny'
+    }
 }
 
-const refreshButton = document.getElementById('refresh');
-refreshButton.addEventListener('click', refresh);
-
-//refresh();
-
-
-// wb.showWeatherData();
-// wb.getCurrentTemp();
-// wb.resetWeather();
-// wb.showWeatherData();
+refresh();
--- weather.js
+++ weather.js
@@ -1,71 +1,89 @@
-// const TEMP = 'temperature_2m';
-const WEATHER_URL = 'https://api.open-meteo.com/v1/forecast?current=temperature_2m&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FDenver&forecast_days=1'
-const GEO_URL = 'https://geocoding-api.open-meteo.com/v1/search?count=1&name='
-const REVERSE_GEOCODE = 'https://us1.api-bdc.net/data/reverse-geocode-client'
+const weatherURL = new URL('https://api.open-meteo.com/v1/forecast?current=temperature_2m,precipitation,wind_speed_10m')
+const geoURL = new URL('https://geocoding-api.open-meteo.com/v1/search?count=1&name=')
+const reverseGeocode = new URL('https://us1.api-bdc.net/data/reverse-geocode-client');
 
 export class WeatherBox {
-    constructor(){
-        this.setCurrentLocationHere = this.setCurrentLocationHere.bind(this);
 
-        this.setCurrentLocationHere();
-        console.log(this.latitude, this.longitude);
-        // this.location = prompt('Where are you?');
-        this.resetWeather();
+    latitude= 39;
+    longitude= 106;
+    locationName= '';
+    cityName='';
+    weatherData = {};
+    tempUnits = 'fahrenheit';
+    windUnits = 'mph';
+
+    constructor () {
+        this.setTempUnits(this.tempUnits);
+        this.setWindUnits(this.windUnits);
+    }
+    
+    setTempUnits = (units) => {
+        this.tempUnits = units;
+        weatherURL.searchParams.set('temperature_unit', units)
+    }
+    
+    setWindUnits = (units) => {
+        this.windUnits = units;
+        weatherURL.searchParams.set('wind_speed_unit', units)
     }
 
-    latitude= 0;
-    longitude= 0;
-    locationName= '';
-    weatherData = { };
+    getPosition = () => {
+        return new Promise((resolve, reject) => 
+            navigator.geolocation.getCurrentPosition(resolve, reject)
+        );
+    }
+
+    setCurrentLocationHere = async () => {
+        let pos = await this.getPosition();
+        this.latitude = pos.coords.latitude
+        this.longitude = pos.coords.longitude
+        weatherURL.searchParams.set('latitude', this.latitude);
+        weatherURL.searchParams.set('longitude', this.longitude);
+        await this.updateLocationName();
+    }
     
-    async setCurrentLocationHere() {
-        navigator.geolocation.getCurrentPosition((pos)=> {
-            this.latitude = pos.coords.latitude
-            this.longitude = pos.coords.longitude
-        });
-        console.log(this.latitude, this.longitude);
-        const reverseGeo = await fetch(REVERSE_GEOCODE + 
+    updateLocationName = async () => {
+        const reverseGeo = await fetch(reverseGeocode + 
             `?latitude=${this.latitude}&longitude=${this.longitude}`);
-        const reverseGeoData = await reverseGeo.json();
-        this.locationName = reverseGeoData.city + ', ' +
-                            reverseGeoData.principalSubdivision + ', ' +
-                            reverseGeoData.countryName;
-        console.log(reverseGeoData);
-        console.log(this.locationName);
+            const reverseGeoData = await reverseGeo.json();
+            this.cityName = reverseGeoData.city;
+            this.locationName = reverseGeoData.city + ', ' +
+            reverseGeoData.principalSubdivision + ', ' +
+            reverseGeoData.countryName;
     }
 
     getLocation(){
         return this.locationName;
     }
 
-    async setLocationByName(locationName){
+    getCityName(){
+        return this.cityName;
+    }
+    
+    setLocationByName = async (locationName) => {
         this.locationName = locationName;
         try{
-            const geo = await fetch(GEO_URL + locationName);
+            const geo = await fetch(geoURL + locationName);
             const geoData = await geo.json();
             this.latitude = geoData.results[0].latitude;
             this.longitude = geoData.results[0].longitude;
-            console.log(geoData)
-            console.log(this.latitude, this.longitude);
+            weatherURL.searchParams.set('latitude', this.latitude);
+            weatherURL.searchParams.set('longitude', this.longitude);
+            await this.updateLocationName();
         } catch(error) {
-            console.error(`ERROR: ${error}`);
+            alert(`ERROR: ${error}\nCould not find that location.`);
+            await this.setCurrentLocationHere();
         }
     }
 
-    async resetWeather(){
-        this.weatherData = await this.loadLatestWeather();
-        console.log('resetWeather called.', this.weatherData);
-    }
-
-    async loadLatestWeather() {
+    loadLatestWeather = async () => {
         try{
-            // await this.getCoordinates();
-            if (this.locationName !== ''){
+            if (this.locationName === ''){
                 await this.setCurrentLocationHere();
             }
-            console.log(this.latitude, this.longitude)
-            const rawWeather = await fetch(WEATHER_URL + `&latitude=${this.latitude}&longitude=${this.longitude}`);
+            const rawWeather = await fetch(weatherURL); 
             const weatherData = await rawWeather.json();
+            this.weatherData = weatherData;
             return weatherData;
         } catch(error) {
             console.error(`ERROR: ${error}`);
@@ -76,20 +94,14 @@ export class WeatherBox {
         try{
             await this.setCurrentLocationHere();
             weatherData = await this.loadLatestWeather();
-            console.log("Weather Data", this.weatherData)
             let temp = weatherData.current.temperature_2m;
-            console.log(temp)
         } catch(error) {
             console.error(`ERROR: ${error}`);
         }
     }
 
-    async showWeatherData(){
-        try{
-            console.log(await this.weatherData)
-        } catch(error) {
-            console.error(`ERROR: ${error}`);
-        }
+    async resetWeather(){
+        this.weatherData = await this.loadLatestWeather();
     }
 }
 
